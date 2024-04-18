2d18516545c717914a1a8f09454cfe37eb5206a5
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-18 15:58:29 +0200

    Fine

--- js/main.js
+++ js/main.js
@@ -11,20 +11,20 @@ const btn = document.querySelector('.btn')
 btn.addEventListener('click', function() {
     console.log(age.value);
     console.log(kilomiters.value);
-    console.log(`Il prezzo è: ${prezzo.toFixed(2)}€`);
     kilomiters.value
     document.getElementById("tkname").innerHTML += names.value;
     document.getElementById("tkage").innerHTML += age.value;
+    let prezzo = (kilomiters.value * base);
+    if (age.value < 18){
+        prezzo = prezzo - (prezzo * 0.2)
+    } else if (age.value > 65){
+        prezzo = prezzo - (prezzo * 0.4)
+    }
     document.getElementById("tiket").innerHTML += (`${prezzo.toFixed(2)}`);
+    console.log(`Il prezzo è: ${prezzo.toFixed(2)}€`);
 })
 
-let prezzo = (kilomiters.value * base)
 
-if (age < 18){
-    prezzo = prezzo - (prezzo * 0.2)
-} else if (age > 65){
-    prezzo = prezzo - (prezzo * 0.4)
-}
 
 
 
